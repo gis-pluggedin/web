c8e63f2860bcec777830783a8742c94f3d68f677
Corey Fields <cfields@gisinc.com>
2014-05-17 21:56:08 -0400

    no message

--- js/main.js
+++ js/main.js
@@ -7,24 +7,46 @@ angular.module('app', [])
 		var map = L.mapbox.map('map', 'jonahadkins.i94mjj8j').setView([37.27, -76.70], 13);
 
         $scope.pm = {
+            activeLayer: undefined,
             layers: [
                 {
-                    id: 0,
                     name: 'Athletic Fields',
-                    url:'data/athleticFields.geojson'
+                    url:'data/athleticFields.geojson',
+                    active: false,
+                    layerRef: undefined
                 },
                 {
-                    id: 1,
-                    name: 'Buildings',
-                    url:'data/buildings.geojson'
+                    name: 'Parks',
+                    url:'data/parks.geojson',
+                    active: false,
+                    layerRef: undefined
                 }
             ],
         };
 
-        $scope.toggleLayer = function(id) {
-            var featureLayer = L.mapbox.featureLayer()
-                .loadURL($scope.pm.layers[id].url)
-                .addTo(map);
+        $scope.toggleLayer = function(index) {
+            if (index === $scope.pm.activeLayer) {
+                return;
+            }
+            else {
+                if ($scope.pm.activeLayer !== undefined) {
+                    map.removeLayer($scope.pm.layers[$scope.pm.activeLayer].layerRef);
+                    $scope.pm.layers[$scope.pm.activeLayer].active = !$scope.pm.layers[$scope.pm.activeLayer].active;
+                }
+
+                $scope.pm.layers[index].layerRef = L.mapbox.featureLayer()
+                    .loadURL($scope.pm.layers[index].url)
+                    .addTo(map);
+
+                $scope.pm.activeLayer = index;
+                $scope.pm.layers[index].active = !$scope.pm.layers[index].active;
+            }
         }
+		
+		function _init() {
+	        $scope.toggleLayer(0);
+		}
+
+		_init();
 	}
 ]);
