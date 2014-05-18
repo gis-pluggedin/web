43ef4dfe3612c2c5154802b5a4f94963fb6d8efd
christopherfricke <christopher.fricke@bmoregeo.com>
2014-05-17 21:26:50 -0400

    add styling to layers

--- js/main.js
+++ js/main.js
@@ -11,20 +11,48 @@ angular.module('app', [])
                 {
                     id: 0,
                     name: 'Athletic Fields',
-                    url:'data/athleticFields.geojson'
-                },
-                {
+                    url:'data/athleticFields.geojson',
+                    active:false,
+                    style:{
+                        "color": "#ff7800",
+                        "weight": 5,
+                        "opacity": 0.65
+                    }
+                },{
+                    id: 0,
+                    name: 'Parks',
+                    url:'data/parks.geojson',
+                    active:true,
+                    style:{
+                        "color": "#348756",
+                        "weight": 5,
+                        "opacity": 0.65
+                    }
+                },{
                     id: 1,
-                    name: 'Buildings',
-                    url:'data/buildings.geojson'
+                    name: 'Voting Districts',
+                    url:'data/votingDistricts.geojson',
+                    active:false,
+                    style:{
+                        "color": "#ff7800",
+                        "weight": 5,
+                        "opacity": 0.65
+                    }
                 }
-            ],
+            ]
         };
 
         $scope.toggleLayer = function(id) {
             var featureLayer = L.mapbox.featureLayer()
-                .loadURL($scope.pm.layers[id].url)
-                .addTo(map);
+                .loadURL($scope.pm.layers[id].url);
+
+            // function to run once marker has loaded
+            featureLayer.on('ready', function(){
+                // navigate the GeoJSON to get to the coordinates
+                var geojson = featureLayer.getGeoJSON();
+
+                L.geoJson(geojson, { style: $scope.pm.layers[id].style }).addTo(map);
+            });
         }
 	}
 ]);
